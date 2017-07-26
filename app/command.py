47d6c55bdeeb46ddee3a46eea8bdaa1f93f096ff
SergioAnd95 <developinc@yadnex.ru>
2017-07-26 15:29:11 +0300

    create command.py - with class Command that represent command in system

---
+++ app/command.py
@@ -0,0 +1,25 @@
+command_list = {}
+
+
+class Command:
+    """
+    Represent command in system
+    """
+    def __init__(self, name, description='', in_help=True):
+        self.name = name.lower()
+        self.in_help = in_help
+        self.description = description
+        self.__keys = [self.name]
+        command_list.append(self)
+
+    @property
+    def keys(self):
+        return self.__keys.copy()
+
+    @keys.setter
+    def keys(self, lst):
+        self.__keys.append([i.lower() for i in lst])
+
+    def process(self, obj):
+        # Function to handle message
+        pass
